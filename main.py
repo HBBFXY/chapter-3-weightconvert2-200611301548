07216651488b1e19ab4310e1d87bc126cb424c1b
200611301548 <3446790582@qq.com>
2025-09-19 09:50:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-# 假设当前在地球上的体重（单位：千克）
 current_weight = 60
-# 每年体重增长（单位：千克）
 annual_growth = 0.5
-# 月球上体重是地球上的比例
 moon_ratio = 0.165
-
-print("未来10年地球和月球上的体重变化：")
+print()
 for year in range(1, 11):
     earth_weight = current_weight + annual_growth * year
     moon_weight = earth_weight * moon_ratio
