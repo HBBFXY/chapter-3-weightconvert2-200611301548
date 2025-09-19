dac9bef7889e900b72d28df5fd81ddc5faab9d4d
200611301548 <3446790582@qq.com>
2025-09-19 09:59:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-current_weight = 60
-annual_growth = 0.5
-moon_ratio = 0.165
+initial_earth_weight = 60  
+lunar_ratio = 0.165  
 print()
 for year in range(1, 11):
-    earth_weight = current_weight + annual_growth * year
-    moon_weight = earth_weight * moon_ratio
-    print(f"第{year}年：地球上体重 {earth_weight:.2f} 千克，月球上体重 {moon_weight:.2f} 千克")
+    earth_weight = initial_earth_weight + 0.5 * year
+    lunar_weight = earth_weight * lunar_ratio
+    print(f"{year}\t{earth_weight:.2f}\t\t{lunar_weight:.2f}")
