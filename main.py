027bf171f38d3af5850f9d5c0dd16427352092c3
200611301548 <3446790582@qq.com>
2025-09-19 10:06:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ initial_earth_weight = 60
 lunar_ratio = 0.165  
 print()
 for year in range(1, 11):
-    earth_weight = initial_earth_weight + 0.5 * year
-    lunar_weight = earth_weight * lunar_ratio
-    print(f"{year}\t{earth_weight:.2f}\t\t{lunar_weight:.2f}")
+   earth_weight=initial_earth_weight+0.5*year
+   lunar_weight=earth_weight*lunar_percent
+   print(f"{year}\t{earth_weight:.2f}\t\t{lunar_weight:.2f}")
